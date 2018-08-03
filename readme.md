5fd684251bce1278436f25fac566f5613f480974
vinicius1889 <vinicius1889@gmail.com>
2018-08-03 14:56:02 -0300

    add: adicionando readme.md atualizado

--- readme.md
+++ readme.md
@@ -1,13 +1,23 @@
-##Exemplo de autenticação  - API ICARROS
+## Exemplo de autenticação  - API ICARROS
 
 
 #### Introdução
 
 Nesse tutorial tentamos mostrar um exemplo simples usando o Apache Oltu. Sendo assim segue um passo a passo de como utilizamos essa dependência para realizar a autenticação.
-
 Por favor, rode a aplicação que foi escrita usando Spring Boot e troque as configurações no arquivo application.properties.
+Existe também um serviço REST para receber o token do callback. 
+Depois desses procedimentos, você estará apto a consumir os serviços da API do [iCarros](https://www.icarros.com.br/apidocs/)
 
-Existe também um serviço REST para receber o token do callback.
+
+
+#### Importante
+
+Por favor, valide as informações de autorização com a nossa equipe (CLIENT_ID, SECRET e URL de CALLBACK). Essas informaçoes devem
+ser configuradas no nosso servidor e serão disponibilizadas.
+Esse exemplo não visa ser um guia definitivo ou algo do tipo. Visamos somente exemplificar usando a linguagem java o fluxo de autorização.
+Para mais informações, por favor veja a documentação ([iCarros](https://www.icarros.com.br/apidocs/apiOauth.html), [OAuth2](https://oauth.net/2/) e [Oltu](https://oltu.apache.org/))
+
+
 
 
 
--- src/test/java/br/com/icarros/exemplo/ExemploApplicationTests.java
+++ src/test/java/br/com/icarros/exemplo/ExemploApplicationTests.java
@@ -4,12 +4,9 @@ import org.apache.oltu.oauth2.client.OAuthClient;
 import org.apache.oltu.oauth2.client.URLConnectionClient;
 import org.apache.oltu.oauth2.client.request.OAuthClientRequest;
 import org.apache.oltu.oauth2.client.response.OAuthJSONAccessTokenResponse;
-import org.apache.oltu.oauth2.common.OAuthProviderType;
 import org.apache.oltu.oauth2.common.exception.OAuthProblemException;
 import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
 import org.apache.oltu.oauth2.common.message.types.GrantType;
-import org.apache.oltu.oauth2.common.token.OAuthToken;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
@@ -45,7 +42,7 @@ public class ExemploApplicationTests {
 				.setClientId(clientID)
 				.setResponseType("code")
 				.setRedirectURI("http://localhost:8089/location")
-				.setScope("offline_access gruporegional  integracaopa anuciantepj usuariosite")
+				.setScope("offline_access anuciantepj")
 				.buildQueryMessage();
 		String locationUri = request.getLocationUri();
 		System.out.println("locationUri = " + locationUri);
@@ -55,10 +52,8 @@ public class ExemploApplicationTests {
 	@Test
 	public void changeToken() throws OAuthSystemException, OAuthProblemException {
 		String url = "https://accounts.icarros.com/auth/realms/icarros/protocol/openid-connect/token";
-		String code = "LwPwIGsUH2D_hKyQ5qzmtla-mfdrtA-26U1whTYU_14.b31734af-7a07-46da-9e51-c94016afcdc5";
-		OAuthClientRequest request =
-										//OAuthClientRequest.tokenLocation("https://accounts.icarros.com/auth/realms/icarros/protocol/openid-connect/token")
-										OAuthClientRequest.tokenLocation(url)
+		String code = "CODE_RETORNADO_AQUI";
+		OAuthClientRequest request =	OAuthClientRequest.tokenLocation(url)
 										.setClientId(clientID)
 										.setClientSecret(secret)
 										.setRedirectURI("http://localhost:8089/location")
@@ -67,13 +62,14 @@ public class ExemploApplicationTests {
 										.buildBodyMessage();
 		OAuthClient oAuthClient = new OAuthClient(new URLConnectionClient());
 		OAuthJSONAccessTokenResponse tokenResponse = oAuthClient.accessToken(request);
-		System.out.println("oAuthJSONAccessTokenResponse = " + tokenResponse.getRefreshToken());
+		System.out.println("ACCESS_TOKEN = " + tokenResponse.getAccessToken());
+		System.out.println("REFRESH_TOKEN  = " + tokenResponse.getRefreshToken());
 	}
 
 	@Test
 	public void refreshToken() throws OAuthSystemException, OAuthProblemException {
 		String url = "https://accounts.icarros.com/auth/realms/icarros/protocol/openid-connect/token";
-		String refreshToken = "eyJhbGciOiJSUzI1NiJ9.eyJqdGkiOiJmYjFmMmU5MC1kM2M3LTQ0YWMtODY2MS0yMTUyNTFmYjA2M2YiLCJleHAiOjAsIm5iZiI6MCwiaWF0IjoxNTMzMjQ1ODAxLCJpc3MiOiJodHRwczovL2FjY291bnRzLmljYXJyb3MuY29tL2F1dGgvcmVhbG1zL2ljYXJyb3MiLCJhdWQiOiJuYnNpLWludGVncmFjYW8iLCJzdWIiOiIwOWY4NmIyNC1lNDM5LTRlMjYtYmFhNy1hZGU1OTFkOWU0ZDIiLCJ0eXAiOiJPZmZsaW5lIiwiYXpwIjoibmJzaS1pbnRlZ3JhY2FvIiwic2Vzc2lvbl9zdGF0ZSI6IjQwZjZhMDkxLTdiZjgtNGM5Ni1iNWQxLTdhYmFhZDgyYjJkZCIsImNsaWVudF9zZXNzaW9uIjoiYjMxNzM0YWYtN2EwNy00NmRhLTllNTEtYzk0MDE2YWZjZGM1IiwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbIm9mZmxpbmVfYWNjZXNzIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiaWNhcnJvcy13ZWJhcHAiOnsicm9sZXMiOlsidXN1YXJpb2ludGVybm8iLCJhZHdvcmRzIiwiYWRtaW4iLCJhZ2VuY2lhIiwiYXRlbnRvIiwiZGFkb3N2ZWljdWxvcyIsImNvYnJhbmNhIiwiY29tZXJjaWFsIiwiZXN0b3F1ZSIsIm9maWNpbmEiLCJ1c3Vhcmlvc2l0ZSIsIm9wZXJhY2FvcGoiLCJvcGVyYWNhb3BmIiwiZmVpcmFvIiwiYW51bmNpYW50ZXBqIiwiY29uc29yY2lvIiwiZmVpcmFvYWRtaW4iLCJsaXN0YWxvamFzIiwiaXRhdSIsImNvbnN1bHRhcmV2ZW5kYXMiLCJtYXJrZXRpbmciLCJwbGFuZWphbWVudG9waiIsImVzdGF0aXN0aWNhcyIsIm5vdGljaWFzIiwicmFpb3giLCJhcGlhY2Nlc3MiLCJwdWJsaWNpZGFkZSIsImNhZGFzdHJvIiwicGxhbmVqYW1lbnRvcGYiXX0sImJyb2tlciI6eyJyb2xlcyI6WyJyZWFkLXRva2VuIl19LCJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50Iiwidmlldy1wcm9maWxlIl19fX0.SmTiwoxOXcyIL_jEF3FJfSdVpqZc8QYGCk4Wu__DxqtQ7klj9-mUOnqjwqVM0iU08d-VmD7hg9jy5n6NvtsZvoLIHySZOECllz-DIi0rJU_PkYJYdhO738uWsR8s82F7HtxcN8TR2e1WYP0W-U5pIHZhdyBq6o_QaulP0ehWxP-Ey_r3ifuz7PbjME__O-_pszC1tsvhOwLHH1nE7ycuTZ_jVxV13GxGAy_JudZJOoxnpUJuE1t4jL20vIrJQjySovY1GlBuWMnmUFANSTviR-tx9v8iTduMZquoVAQey-PCR8Zrgi3-xug6Pdah_9bSDL0oorKUGSRavuuAhdg4Hg";
+		String refreshToken = "REFRESH_TOKEN_AQUI";
 
 		OAuthClientRequest request =
 										OAuthClientRequest.tokenLocation(url)
